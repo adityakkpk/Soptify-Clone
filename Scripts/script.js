73d5500836ca3e215a9867c61cb89e53a2aeaec2
Aditya Kumar Kushwaha <91123318+adityakkpk@users.noreply.github.com>
2023-12-31 16:03:29 +0530

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -19,7 +19,7 @@ async function getSongs(){
     let response = await a.text();
     let div = document.createElement('div');
     div.innerHTML = response;
-    let aElms = div.querSelector('.react-directory-truncate a');
+    let aElms = div.getElementsByTagName('a');
     let songs = [];
     for (let index = 0; index < aElms.length; index++) {
         const element = aElms[index];
